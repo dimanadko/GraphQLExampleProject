44827dd479bc2ea6fc4701beb898786f2ec2f4b1
@dimanadko <dimanadobko@gmail.com>
2019-09-06 19:07:53 +0300

    feat(server): implement db connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const graphqlHHTP = require("express-graphql");
+const mongoose = require("mongoose");
 
 const schema = require("./schema/schema");
 
 const app = express();
+
+const password = process.env.MONGO_GRAPHQL_EXAMPLE_PROJECT_PASSWORD;
+
+mongoose.connect(
+  `mongodb+srv://admin:${password}@cluster0-5zsa6.azure.mongodb.net/test?retryWrites=true&w=majority`
+);
+mongoose.connection.once("open", () => console.log("connected to database"));
 
 app.use("/graphql", graphqlHHTP({ schema, graphiql: true }));
 
